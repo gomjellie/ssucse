db3a04c0f7da2c14b5d2c1ff865fe094b07bc8b6
gomjellie <gomjellie@gmail.com>
2020-09-19 02:06:06 +0900

    lrparser 실수 처리 완료

--- Compiler/HW2/lr_parser.c
+++ Compiler/HW2/lr_parser.c
@@ -71,9 +71,6 @@ int prod_length[7] = {
 };
 
 int stack[1000]; int top = -1; int sym;
-int value[1000];
-char yytext[32];
-int yylval;
 
 typedef struct {
     size_t counter;
@@ -98,6 +95,16 @@ typedef struct number {
     real_number_t data;
 } number_t;
 
+number_t value[1000];
+char yytext[32];
+number_t yylval;
+NUMBER_TYPE yytype;
+
+number_t number_parse(const char *text);
+number_t number_plus(number_t n1, number_t n2);
+number_t number_mul(number_t n1, number_t n2);
+void number_print(const number_t *this);
+
 int yylex();
 void yyparse();
 void push(int i);
@@ -120,11 +127,13 @@ void yyparse() {
         i = action[stack[top]][sym]; // get relation
         if (i == ACC) {
             puts("success !");
-            printf("evaluated result: %d\n", value[1]);
+            printf("evaluated result: ");
+            number_print(&value[1]);
+            // printf("evaluated result: %d\n", value[1]);
         }
         else if (i > 0) shift(i);
         else if (i < 0) reduce(-i);
-        else error("yyerror!");
+        else error("yyerror! got wrong syntax here");
     } while (i != ACC);
 }
 
@@ -145,11 +154,12 @@ void reduce(int i) {
     push(go_to[stack[old_top]][prod_left[i]]);
     switch (i) {
         case 1:
-        value[top] = value[old_top + 1] + value[old_top + 3]; break;
+        value[top] = number_plus(value[old_top + 1], value[old_top + 3]); break;
         case 2:
         value[top] = value[old_top + 1]; break;
         case 3:
-        value[top] = value[old_top + 1] * value[old_top + 3];
+        value[top] = number_mul(value[old_top + 1], value[old_top + 3]); break;
+        // value[top] = value[old_top + 1] * value[old_top + 3];
         case 4:
         value[top] = value[old_top + 1]; break;
         case 5:
@@ -172,14 +182,16 @@ int yylex() {
     static char ch = ' ';
     int i = 0;
     while (ch == ' ' || ch == '\t') ch = _getchar();
-    
+    yytype = INTEGER;
+
     if (isdigit(ch)) {
         do {
             yytext[i++] = ch;
             ch = _getchar();
-        } while (isdigit(ch));
+            if (ch == '.') yytype = FLOAT;
+        } while (isdigit(ch) || ch == '.');
         yytext[i] = 0;
-        yylval = atoi(yytext);
+        yylval = number_parse(yytext);
         return NUMBER;
     }
     else if (ch == '+') { ch = _getchar(); return PLUS; }
@@ -204,4 +216,44 @@ void error_handler_init(error_handler_t *this) {
 
 void error_handler_count() {
     error_handler.counter ++;    
+}
+
+number_t number_parse(const char *text) {
+    if (yytype == FLOAT) {
+        return (number_t) { .type = FLOAT, .data.float_val = atof(yytext) };
+    }
+    return (number_t) { .type = INTEGER, .data.int_val = atoi(yytext) };
+}
+
+number_t number_plus(number_t n1, number_t n2) {
+    double double_sum = 0;
+    int int_sum = 0;
+    if (n1.type == FLOAT) double_sum += n1.data.float_val;
+    if (n2.type == FLOAT) double_sum += n2.data.float_val;
+    if (n1.type == INTEGER) int_sum += n1.data.int_val;
+    if (n2.type == INTEGER) int_sum += n2.data.int_val;
+
+    if (n1.type == FLOAT || n2.type == FLOAT)
+        return (number_t) { .type = FLOAT, .data.float_val = double_sum + int_sum };
+    return (number_t) { .type = INTEGER, .data.int_val = int_sum };
+}
+
+number_t number_mul(number_t n1, number_t n2) {
+    double double_mul = 0;
+    int int_mul = 0;
+    if (n1.type == FLOAT) {
+        if (n2.type == FLOAT)
+            return (number_t) {.type = FLOAT, .data.float_val = n1.data.float_val * n2.data.float_val };
+        return (number_t) {.type = FLOAT, .data.float_val = n1.data.float_val * n2.data.int_val };
+    }
+    if (n2.type == FLOAT)
+        return (number_t) {.type = FLOAT, .data.float_val = n1.data.int_val * n2.data.float_val };
+    return (number_t) {.type = INTEGER, .data.int_val = n1.data.int_val * n2.data.int_val };
+}
+
+void number_print(const number_t *this) {
+    if (this->type == INTEGER) {
+        printf("%d", this->data.int_val); return;
+    }
+    printf("%.2f", this->data.float_val);
 }
