d34aa8849ffd36bbad342bd7bdbdd591e2c583b7
gomjellie <gomjellie@naver.com>
2020-11-04 14:44:59 +0900

    print 오타 수정(과제 pdf가 {}잘못나옴), lex에서 리터럴 처리 추가

--- Compiler/MID-PROJECT/print.c
+++ Compiler/MID-PROJECT/print.c
@@ -311,9 +311,9 @@ void prt_A_TYPE(A_TYPE *t, int s)
             printf("| INDEX\n");
             if (t->expr)
                 prt_expression(t->expr, s + 2);
-            else
-                print_space(s + 2);
-            printf("(none)\n");
+            else {
+                print_space(s + 2); printf("(none)\n");
+            }
             print_space(s);
             printf("| ELEMENT_TYPE\n");
             prt_A_TYPE(t->element_type, s + 2);
