afcd6cf5c2625a147695c5f0bda3394383ff7de1
gomjellie <gomjellie@gmail.com>
2020-12-05 04:12:12 +0900

    post 수정하기 ui구현 완료

--- Web Programming/Final Project/front/src/Board.js
+++ Web Programming/Final Project/front/src/Board.js
@@ -1,29 +1,72 @@
 import React from 'react';
 
-import { PanelGroup, Panel, Divider, FlexboxGrid, Button, Modal } from 'rsuite';
+import { PanelGroup, Panel, Divider, FlexboxGrid, Button, Modal, Input } from 'rsuite';
 
-const Board = ({ posts, onPostEdit, onPostDelete }) => {
-  const onPost = () => () => onPostEdit(post._id, post.content);
+class Board extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      show: false,
+      content: "",
+      post: {},
+    };
+    this.close = this.close.bind(this);
+    this.open = this.open.bind(this);
+    this.onContentChange = this.onContentChange.bind(this);
+  }
 
-  return (
-    <PanelGroup>
-      <div style={{ width: 500 }}>
-        {posts.map((post, index) => (
-          <Panel header={`${post.title} - ${post.writerName} - ${post.createdAt.substr(0, 10)}`} collapsible bordered>
-            {post.content}
-            <Divider />
-            <FlexboxGrid justify="end">
-              <FlexboxGrid.Item>
-                <Button size="xs" appearance="link" active onClick={onPost}>수정하기</Button>
-                <Divider vertical />
-                <Button size="xs" appearance="link" active onClick={() => onPostDelete(post._id)}>삭제하기</Button>
-              </FlexboxGrid.Item>
-            </FlexboxGrid>
-          </Panel>
-        ))}
+  onContentChange(value) {
+    this.setState({ content: value });
+  }
+
+  close() {
+    this.setState({ show: false });
+  }
+
+  open(post) {
+    this.setState({ show: true, post });
+  }
+
+  render() {
+    const { posts, onPostEdit, onPostDelete } = this.props;
+
+    return (
+      <div>
+        <PanelGroup>
+          <div style={{ width: 500 }}>
+            {posts.map((post, index) => (
+              <Panel header={`${post.title} - ${post.writerName} - ${post.createdAt.substr(0, 10)}`} collapsible bordered>
+                {post.content}
+                <Divider />
+                <FlexboxGrid justify="end">
+                  <FlexboxGrid.Item>
+                    <Button size="xs" appearance="link" active onClick={() => this.open(post)}>수정하기</Button>
+                    <Divider vertical />
+                    <Button size="xs" appearance="link" active onClick={() => onPostDelete(post._id)}>삭제하기</Button>
+                  </FlexboxGrid.Item>
+
+                </FlexboxGrid>
+              </Panel>
+            ))}
+          </div>
+        </PanelGroup>
+        <Modal show={this.state.show} onHide={this.close}>
+          <Modal.Header>
+            <Modal.Title>{this.state.post.title}</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <Input onChange={this.onContentChange} name="content" rows={5} componentClass="textarea">
+              {this.state.post.content}
+            </Input>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button onClick={() => onPostEdit(this.state.post._id, this.state.content)} appearance="primary">수정하기</Button>
+            <Button onClick={this.close} appearance="subtle">취소</Button>
+          </Modal.Footer>
+        </Modal>
       </div>
-    </PanelGroup>
-  );
-}
+    )
+  }
+};
 
 export default Board;
